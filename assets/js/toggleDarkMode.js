53c657390e9657e4557215343629bcca6ed007a1
Goh Jia Wei <goh_jia_wei@hotmail.com>
2024-03-23 17:34:52 +0800

    Add dark mode toggle button

---
+++ assets/js/toggleDarkMode.js
@@ -0,0 +1,25 @@
+const changeTheme = document.getElementById('mode');
+
+// on page load
+const mode = window.localStorage.getItem('mode');
+if (mode == 'dark') {
+  changeTheme.checked = true;
+  jtd.setTheme("dark");
+}
+
+if (mode == 'light') {
+  changeTheme.checked = false;
+  jtd.setTheme("mytheme");
+}
+
+// toggles dark mode
+const theme = document.getElementById('theme');
+changeTheme.onchange = (e) => {
+  if (changeTheme.checked === true) {
+    jtd.setTheme("dark")
+    window.localStorage.setItem('mode', 'dark');
+  } else {
+    jtd.setTheme("mytheme")
+    window.localStorage.setItem('mode', 'light');
+  }
+}
