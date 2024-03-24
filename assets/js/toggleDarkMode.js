ad8813e64a0c15e5fd544eca79024fae9f4fa44f
Goh Jia Wei <goh_jia_wei@hotmail.com>
2024-03-24 10:45:38 +0800

    Simplify toggleDarkMode.js; update tag label to change colour for dark mode

--- assets/js/toggleDarkMode.js
+++ assets/js/toggleDarkMode.js
@@ -2,24 +2,25 @@ const changeTheme = document.getElementById('mode');
 
 // on page load
 const mode = window.localStorage.getItem('mode');
-if (mode == 'dark') {
+if (mode != null && mode == 'dark') {
   changeTheme.checked = true;
-  jtd.setTheme("mydarktheme");
-}
-
-if (mode == 'light') {
+} else {
   changeTheme.checked = false;
-  jtd.setTheme("mytheme");
 }
 
-// toggles dark mode
-const theme = document.getElementById('theme');
-changeTheme.onchange = (e) => {
+function toggleDarkMode(){
   if (changeTheme.checked === true) {
     jtd.setTheme("mydarktheme")
     window.localStorage.setItem('mode', 'dark');
+    document.body.style.setProperty("--labelColor", "#7253ed")
   } else {
     jtd.setTheme("mytheme")
     window.localStorage.setItem('mode', 'light');
+    document.body.style.setProperty("--labelColor", "#DC143C")
   }
 }
+// toggles dark mode
+const theme = document.getElementById('theme');
+changeTheme.onchange = (e) => {toggleDarkMode()};
+
+toggleDarkMode();
